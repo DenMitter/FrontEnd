16c71c1663dbf917fdab2b54792fe06a1a9e6b68
unknown <57758gfk@gmail.com>
2022-12-20 11:02:01 +0300

    Add JS

---
+++ BG and Form/js/scripts.js
@@ -0,0 +1,5 @@
+const backgroundSection = document.getElementById('bgElem');
+for(i = 0; i < 145; i++) {
+    const elem = document.createElement('span');
+    backgroundSection.parentNode.appendChild(elem);
+}
